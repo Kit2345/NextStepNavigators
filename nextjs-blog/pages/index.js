e015dec808d0cb43e33a8a68df96b5917a03e6d5
Wing-Kit Leung <wing_kitleung@hotmail.com>
2024-08-18 16:38:08 +0100

    the answers now only appear after answering all the questions. Space between input box and button

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -38,7 +38,7 @@ export default function Home() {
   //   getData();
   // }, []);
 
-  const [currentQuestion, setCurrentQUestion] = useState(
+  const [currentQuestion, setCurrentQuestion] = useState(
     "What subjects or activities do you enjoy the most in school or during your free time?"
   );
 
@@ -56,68 +56,93 @@ export default function Home() {
     event.preventDefault();
     console.log("submitted");
     setCount(count + 1);
-    if (count >= 5) {
+    if (count > 4) {
       setCount(0);
     }
-    setCurrentQUestion(questionArray[count]);
-    console.log(count);
+    setCurrentQuestion(questionArray[count]);
+    console.log("count", count);
+  }
+
+  function handleClick(event) {
+    event.preventDefault();
+    console.log("clicked");
+    setCount(0);
   }
 
   return (
+    // if user has not answered all questions:
+
     <div className="bg-[#00637F] ">
       <div>
         <img src="/pictures/image.png" className="max-w-full"></img>
       </div>
       <div className="max-w-4xl m-auto text-3xl/relaxed ">
-        <p className="text-white py-8">
-          Are you looking for a new job? I'd be happy to help you find a career
-          path. Let's start by learning a bit more about your interests and
-          skills. I'll ask you a few questions to get a better idea of what you
-          might enjoy. Ready?
-        </p>
-        <form
-          onSubmit={handleSubmit}
-          className="pt-4 pb-8 m-4 bg-[#EE9800] px-8 rounded-3xl"
-        >
-          <p className="text-black py-4">{currentQuestion}</p>
-          <input
-            type="text"
-            className="bg-white rounded-3xl p-8 min-w-full"
-            placeholder="Your Answer"
-          />
-          <button className="bg-[#00637F] text-white rounded-3xl p-4 cursor-pointer hover:bg-white hover:text-black border border-transparent hover:border-black">
-            Submit
-          </button>
-        </form>
-
-        <div className="mt-24 mb-16">
-          <p className="text-white">
-            Fantastic! Based on your interests in working with numbers, enjoying
-            collaboration, preferring a structured environment, wanting to help
-            people by solving problems, and having an interest in science and
-            young people, here are three career options that might suit you:
-          </p>
-        </div>
-        <div className="text-white ml-32">
-          <ul className="space-y-8">
-            <li>
-              <b> &rArr; Data Analyst:</b> This role involves working with data
-              and numbers to find insights and solve problems. It's structured
-              and often involves collaboration.
-            </li>
-            <li>
-              <b> &rArr; Health and Safety Coordinator:</b> In this role, you
-              can ensure safe environments for people, which can be particularly
-              important in scientific settings like labs or schools. This
-              position also involves problem-solving and helping others.
-            </li>
-            <li>
-              <b> &rArr; Lab Technician:</b> Working in a scientific
-              environment, you can assist in experiments and research, helping
-              solve scientific problems in a structured setting.
-            </li>
-          </ul>
-        </div>
+        {count < 5 ? (
+          <>
+            <p className="text-white py-8">
+              Are you looking for a new job? I'd be happy to help you find a
+              career path. Let's start by learning a bit more about your
+              interests and skills. I'll ask you a few questions to get a better
+              idea of what you might enjoy. Ready?
+            </p>
+            <form
+              onSubmit={handleSubmit}
+              className="pt-4 pb-8 m-4 bg-[#EE9800] px-8 rounded-3xl"
+            >
+              <p className="text-black py-4">{currentQuestion}</p>
+              <input
+                type="text"
+                className="bg-white rounded-3xl p-8 min-w-full"
+                placeholder="Your Answer"
+              />
+              <button className="mt-8 bg-[#00637F] text-white rounded-3xl p-4 cursor-pointer hover:bg-white hover:text-black border border-transparent hover:border-black">
+                Submit
+              </button>
+            </form>
+          </>
+        ) : (
+          <>
+            <div className="mt-24 mb-16">
+              <p className="text-white">
+                Fantastic! Based on your interests in working with numbers,
+                enjoying collaboration, preferring a structured environment,
+                wanting to help people by solving problems, and having an
+                interest in science and young people, here are three career
+                options that might suit you:
+              </p>
+            </div>
+            <div className="text-white ml-32">
+              <ul className="space-y-8">
+                <li>
+                  <b> &rArr; Data Analyst:</b> This role involves working with
+                  data and numbers to find insights and solve problems. It's
+                  structured and often involves collaboration.
+                </li>
+                <li>
+                  <b> &rArr; Health and Safety Coordinator:</b> In this role,
+                  you can ensure safe environments for people, which can be
+                  particularly important in scientific settings like labs or
+                  schools. This position also involves problem-solving and
+                  helping others.
+                </li>
+                <li>
+                  <b> &rArr; Lab Technician:</b> Working in a scientific
+                  environment, you can assist in experiments and research,
+                  helping solve scientific problems in a structured setting.
+                </li>
+              </ul>
+            </div>
+            <div className="pt-4 pb-8 m-4 bg-[#EE9800] px-8 rounded-3xl">
+              If none of the options appeal please click button to reset
+              <button
+                onClick={handleClick}
+                className="bg-[#00637F] text-white rounded-3xl p-4 cursor-pointer hover:bg-white hover:text-black border border-transparent hover:border-black"
+              >
+                Reset
+              </button>
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
